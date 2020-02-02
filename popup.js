4db202a3e668e60efa5fc0476a1bff66b3324667
Saket <saketpkulkarni@gmail.com>
2020-02-02 20:33:32 +0530

    Extension complete code

---
+++ popup.js
@@ -0,0 +1,21 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+	var copyTokenButton = document.getElementById( 'copyTokenButton' );
+	copyTokenButton.addEventListener( 'click', function () {
+		chrome.tabs.executeScript( null, {
+			code: "window.getSelection().toString();"
+		}, function ( selection ) {
+			var accessToken = JSON.parse( selection[ 0 ] )[ 'access_token' ];
+			console.log( accessToken );
+			var dummy = document.createElement( 'textarea' );
+			document.body.appendChild( dummy );
+			dummy.value = accessToken;
+			dummy.select();
+			document.execCommand( 'copy' );
+			document.body.removeChild( dummy );
+
+			// Show Success Message
+			copyTokenButton.classList.add( 'displayNone' );
+			document.getElementById( 'successMsg' ).classList.remove( 'displayNone' );
+		} );
+	}, false );
+}, false );
